bc8d853634fc09b4116c505de1a84a28347b145f
song <276959848@qq.com>
2021-01-27 11:02:20 +0800

    create a demo sln

---
+++ Skany.Output/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skany.Output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
